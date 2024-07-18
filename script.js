ffbeb7826d251a8efc2a294089a2d69eaa536523
NotaCoderDave <111663726+NotaCoderDave@users.noreply.github.com>
2024-07-17 23:34:52 -0400

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// JavaScript for slideshow functionality
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
 function showNextSlide() {
-    slides[currentSlide].classList.remove('active');
+    slides[currentSlide].style.opacity = 0;
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
+    slides[currentSlide].style.opacity = 1;
 }
 
-setInterval(showNextSlide, 3000); // Change slide every 3 seconds
+// Initial slide display
+slides[currentSlide].style.opacity = 1;
+
+// Change slide every 3 seconds
+setInterval(showNextSlide, 3000);
